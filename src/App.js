45bb1512a6a85ccdb92fbe9b89aaf46a620fdffa
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-02 15:37:37 +0500

    Input handling functionality is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [toDoList, setToDoList] = useState([])
+  const [newTask, setNewTask] = useState('');
+  const handleChange = (e) => {
+    setNewTask(e.currentTarget.value);
+  }
+
   return (
-    <div>App</div>
+    <div className='App'>
+        <div className='addTask'>
+            <input onChange={handleChange} />
+            <button>Add Task</button>
+        </div>
+        <div className='list'>
+          {newTask}
+        </div>
+    </div>
   )
 }
 
